0bc7ff5b29e0b6f8b2e681329729b0b410eec334
cagim125 <cagim125@gmail.com>
2024-08-14 14:48:05 +0900

    패스워드 인코더 주입 및 회원가입 시 이미 존재하는 아이디 Exception 처리

--- src/main/java/com/sanplink/api/user/UserService.java
+++ src/main/java/com/sanplink/api/user/UserService.java
@@ -1,7 +1,10 @@
 package com.sanplink.api.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,17 +14,23 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public List<User> getUser() {
         return userRepository.findAll();
     }
 
     public User registerUser(UserDto userDto) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        userDto.setPassword(encoder.encode(userDto.getPassword()));
-        User user = new User(userDto);
+        try {
+            userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+            User user = new User(userDto);
 
-        return userRepository.save(user);
+            return userRepository.save(user);
+
+        } catch (DataIntegrityViolationException e) {
+
+            throw new RuntimeException("Already use username.");
+        }
     }
 
 }
