f3933f37fd35a59be0d3706c68a49abb93ee1a54
cagim125 <cagim125@gmail.com>
2024-08-14 23:38:09 +0900

    로그인 시 필요한 UserDetailService 추가

---
+++ src/main/java/com/sanplink/api/user/MyUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.sanplink.api.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       Optional<User> result = userRepository.findByUsername(username);
+       if (result.isEmpty()) {
+           throw new UsernameNotFoundException("그런아이디 없음");
+       }
+        User user = result.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("일반유저"));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+
+}
