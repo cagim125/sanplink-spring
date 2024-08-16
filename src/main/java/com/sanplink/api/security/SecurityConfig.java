24018d039282b931163883956e04d18d39edd785
cagim125 <cagim125@gmail.com>
2024-08-17 00:30:09 +0900

    AuthenticationManager Bean 생성

--- src/main/java/com/sanplink/api/security/SecurityConfig.java
+++ src/main/java/com/sanplink/api/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.sanplink.api.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +21,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable());
         http.authorizeHttpRequests((authorize) ->
@@ -28,7 +36,13 @@ public class SecurityConfig {
                 .failureUrl("http://localhost:3000/login")
 
         );
-        http.logout(logout -> logout.logoutUrl("/logout"));
+        http.logout(logout -> logout
+                .logoutUrl("/api/logout")
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.setStatus(HttpServletResponse.SC_OK);
+                })
+        );
+
 
         return http.build();
     }
