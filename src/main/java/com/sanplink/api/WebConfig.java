a4f54b8757700b0e9d94601e8a58a19509c5c044
cagim125 <cagim125@gmail.com>
2024-08-17 00:25:26 +0900

    React CORS 설정

---
+++ src/main/java/com/sanplink/api/WebConfig.java
@@ -0,0 +1,17 @@
+package com.sanplink.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React 앱의 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 쿠키 허용
+    }
+}
