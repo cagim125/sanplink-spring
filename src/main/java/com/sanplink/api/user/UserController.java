5cbd1621ab024a502bbd954d44c7c6c32a1cb9c6
cagim125 <cagim125@gmail.com>
2024-08-17 00:28:04 +0900

    @RequestMapping /api/로 변경

--- src/main/java/com/sanplink/api/user/UserController.java
+++ src/main/java/com/sanplink/api/user/UserController.java
@@ -1,20 +1,32 @@
 package com.sanplink.api.user;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.parser.Cookie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
+    private final AuthenticationManager authenticationManager;
+
+    @GetMapping("check")
+    public String check() {
+        return "Suceess";
+    }
 
     @GetMapping
     public List<User> getUser() {
@@ -27,28 +39,40 @@ public class UserController {
     public User registredUser(@RequestBody UserDto userDto) {
         return userService.registerUser(userDto);
     }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(
-//            @RequestParam String username ,
-//            @RequestParam String password
-//    ) {
-//        if (username != null && password != null) {
-//            return ResponseEntity.ok("데이터 잘 넘어옴");
-//        }
-//
-//        return ResponseEntity.ok("데이터 잘 안 넘어옴");
-//    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserLoginRequest userLoginRequest) {
+
+        try{
+            // UsernamePasswordAuthenticationToken을 사용해 사용자를 인증
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword())
+            );
+            // 인증 성공 시
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            // username, password, authority 값을 저장할 객체
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+
+
+            return ResponseEntity.ok(userDetails);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+
+    }
+
 
     @GetMapping("/my-page")
-    public Authentication myPage(Authentication auth) {
+    public void myPage(Authentication auth) {
 
         System.out.println(auth);
 //        System.out.println(auth.getName());
         System.out.println("isAuth : " + auth.isAuthenticated());
 //        System.out.println(auth.getAuthorities());
 
-        return auth;
+//        return auth;
     }
 
     @ExceptionHandler(RuntimeException.class)
