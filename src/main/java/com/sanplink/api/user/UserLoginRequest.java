7dcf629e9637fba46867dca9f543b3f03953f22a
cagim125 <cagim125@gmail.com>
2024-08-17 00:26:56 +0900

    로그인 dto

---
+++ src/main/java/com/sanplink/api/user/UserLoginRequest.java
@@ -0,0 +1,15 @@
+package com.sanplink.api.user;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequest {
+
+    private String username;
+    private String password;
+
+
+}
