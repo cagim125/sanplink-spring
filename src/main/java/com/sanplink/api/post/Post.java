e492d0dedfe44fb48d77b7bab75257d2febd7201
cagim125 <cagim125@gmail.com>
2024-08-20 01:21:16 +0900

    Post Entity 생성 및, Post JpaRepo 설정

---
+++ src/main/java/com/sanplink/api/post/Post.java
@@ -0,0 +1,24 @@
+package com.sanplink.api.post;
+
+import com.sanplink.api.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String content;
+    private String imageUrl;
+
+}
