98c840b117b0aa9187a91c0b6a0eb019d11d4747
cagim125 <cagim125@gmail.com>
2024-08-20 01:17:53 +0900

    패키지명 변경

--- src/main/java/com/sanplink/api/security/WebConfig.java
+++ src/main/java/com/sanplink/api/security/WebConfig.java
@@ -1,4 +1,4 @@
-package com.sanplink.api;
+package com.sanplink.api.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
