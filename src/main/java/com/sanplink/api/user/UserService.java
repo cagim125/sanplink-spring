837a2204a16111ce58196bf223310756735f227e
cagim125 <cagim125@gmail.com>
2024-08-20 01:19:09 +0900

    Mypage 처리를 위한 getUser, 회원가입 처리 중 profileImg null 처리

--- src/main/java/com/sanplink/api/user/UserService.java
+++ src/main/java/com/sanplink/api/user/UserService.java
@@ -1,6 +1,8 @@
 package com.sanplink.api.user;
 
+import com.sanplink.api.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,24 +18,50 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public List<User> getUser() {
-        return userRepository.findAll();
+    // get user
+    public ResponseDto<?> getUser(String username) {
+        User result = userRepository.findByUsername(username).get();
+        result.setPassword("");
+        return ResponseDto.setSuccessData("성공", result);
     }
 
-    public User registerUser(UserDto userDto) {
+    // Signup
+    public ResponseDto<?> signUp(UserDto userDto) {
+        String username = userDto.getUsername();
+
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
+
         try {
-            userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
-            User user = new User();
-            user.setUsername(userDto.getUsername());
-            user.setPassword(userDto.getPassword());
-            user.setEmail(userDto.getEmail());
-
-            return userRepository.save(user);
-
-        } catch (DataIntegrityViolationException e) {
-
-            throw new RuntimeException("Already use username.");
+            if (userRepository.existsByUsername(username)) {
+                return ResponseDto.setFailed("이미 존재하는 아이디입니다.");
+            }
+        } catch (Exception e) {
+            return ResponseDto.setFailed("DB 연결 실패");
         }
+
+        User user = new User(userDto);
+
+        if(user.getProfileImageUrl() == ""){
+            user.setProfileImageUrl("https://spring-test-bucket-123.s3.ap-northeast-2.amazonaws.com/user/anonymous.png");
+        }
+
+        try {
+
+            User result = userRepository.save(user);
+
+            if (result == null) {
+                return ResponseDto.setSuccess("회원가입 실패");
+            }
+
+        } catch (Exception e) {
+            return ResponseDto.setFailed("DB 연결 실패");
+
+        }
+
+        user.setPassword("");
+
+        return ResponseDto.setSuccessData("회원가입 성공", user);
     }
 
 }
