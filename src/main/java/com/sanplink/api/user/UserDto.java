55d0efbe793215ae95c267a6598251106f4547f6
cagim125 <cagim125@gmail.com>
2024-08-20 01:17:27 +0900

    UserDto profileImageUrl 추가

--- src/main/java/com/sanplink/api/user/UserDto.java
+++ src/main/java/com/sanplink/api/user/UserDto.java
@@ -2,12 +2,15 @@ package com.sanplink.api.user;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class UserDto {
 
     private String username;
     private String email;
     private String password;
+    private String profileImageUrl;
 }
