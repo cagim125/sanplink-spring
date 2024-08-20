a07dd9c3536cccec23fcf7bafbf9b1bf3553f5c1
cagim125 <cagim30@gmail.com>
2024-08-20 21:14:34 +0900

    postDto, user 생성자

--- src/main/java/com/sanplink/api/post/Post.java
+++ src/main/java/com/sanplink/api/post/Post.java
@@ -1,13 +1,18 @@
 package com.sanplink.api.post;
 
+import com.sanplink.api.dto.PostDto;
 import com.sanplink.api.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
 
     @Id
@@ -20,5 +25,11 @@ public class Post {
 
     private String content;
     private String imageUrl;
+
+    public Post(PostDto postDto, User user){
+        this.content = postDto.getContent();
+        this.imageUrl = postDto.getImageUrl();
+        this.user = user;
+    }
 
 }
