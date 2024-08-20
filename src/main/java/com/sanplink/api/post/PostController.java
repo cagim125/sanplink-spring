dbb052f35d4d45104d7ce63d71bf5b239139c9a9
cagim125 <cagim30@gmail.com>
2024-08-20 22:41:45 +0900

    post 이미지 업로드url 생성 api 추가

--- src/main/java/com/sanplink/api/post/PostController.java
+++ src/main/java/com/sanplink/api/post/PostController.java
@@ -1,19 +1,25 @@
 package com.sanplink.api.post;
 
+import com.sanplink.api.config.S3Service;
 import com.sanplink.api.dto.PostDto;
 import com.sanplink.api.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService postService;
+    private final S3Service s3Service;
 
+
+    @GetMapping("/presigned-url")
+    @ResponseBody
+    public String getUrl(@RequestParam String filename) {
+        String result = s3Service.createPresignedUrl("post/" + filename);
+        return result;
+    }
 
     @PostMapping("/save")
     public ResponseDto<?> savePost(@RequestBody PostDto requestDto) {
